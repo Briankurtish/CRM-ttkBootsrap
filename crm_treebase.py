0d4e4cf986f5459f23a713e56520b95caed1b6f1
cipher <asaforbrian18@gmail.com>
2023-08-04 09:17:41 +0300

    Created 2 menubuttons and a top bar and some config for the menubutton color

--- crm_treebase.py
+++ crm_treebase.py
@@ -1,10 +1,27 @@
 import tkinter as tk
-from ttkbootstrap.constants import *
 import ttkbootstrap as ttk
 
 root = ttk.Window(themename="superhero")
 root.title("CRM Treeview TTkBootstrap")
 root.geometry("1000x550")
+
+# Set the ttkbootstrap theme to 'superhero'
+style = ttk.Style()
+style.theme_use('superhero')
+
+# Define a custom style for the Menubutton
+style.configure('Custom.TMenubutton', foreground='white')
+
+# buttonbar
+buttonbar = ttk.Frame(root, style='primary.TFrame')
+buttonbar.pack(fill=tk.X, pady=1, side=tk.TOP)
+
+# Menubutton in the button bar
+options_menu = ttk.Menubutton(buttonbar, bootstyle="outline", text="Options",style='Custom.TMenubutton')
+options_menu.pack(side=tk.LEFT, padx=(10, 5), pady=5)
+
+search_menu = ttk.Menubutton(buttonbar, bootstyle="outline", text="Search", style='Custom.TMenubutton')
+search_menu.pack(side=tk.LEFT, padx=(10, 5), pady=5)
 
 
 root.mainloop()
