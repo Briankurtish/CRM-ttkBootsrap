227c6268131a8924a364ef8f2c493669fe0314ad
cipher <asaforbrian18@gmail.com>
2023-08-04 08:49:11 +0300

    Created the file with starting code

--- crm_treebase.py
+++ crm_treebase.py
@@ -1,6 +1,10 @@
 import tkinter as tk
+from ttkbootstrap.constants import *
 import ttkbootstrap as ttk
 
-root = tk.Tk()
+root = ttk.Window(themename="superhero")
+root.title("CRM Treeview TTkBootstrap")
+root.geometry("1000x550")
+
 
 root.mainloop()
